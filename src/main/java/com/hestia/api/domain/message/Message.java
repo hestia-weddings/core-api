6d41e32299f1e18e6658c54b882e9ffaed069152
jeperico <joaoeperico@gmail.com>
2026-04-27 11:26:53 -0300

    feat: [message] create message entity

---
+++ src/main/java/com/hestia/api/domain/message/Message.java
@@ -0,0 +1,42 @@
+package com.hestia.api.domain.message;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "messages")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String sender;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String message;
+
+    @Column(name = "is_favorite", nullable = false)
+    private Boolean isFavorite;
+
+    @Column(name = "is_new", nullable = false)
+    private Boolean isNew;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Column(name = "is_active", nullable = false)
+    private Boolean isActive;
+}
