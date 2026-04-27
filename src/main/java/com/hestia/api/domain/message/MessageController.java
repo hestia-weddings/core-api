fdbc3a661db5834bfccf973e3414deeb281f82b2
jeperico <joaoeperico@gmail.com>
2026-04-27 16:05:36 -0300

    feat: [message] create message controller

---
+++ src/main/java/com/hestia/api/domain/message/MessageController.java
@@ -0,0 +1,44 @@
+package com.hestia.api.domain.message;
+
+import com.hestia.api.domain.message.dto.CreateMessageRequest;
+import com.hestia.api.domain.message.dto.MessageResponse;
+import com.hestia.api.domain.message.dto.UpdateMessageRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/message")
+@RequiredArgsConstructor
+public class MessageController {
+
+    private MessageService messageService;
+
+    @GetMapping
+    public List<MessageResponse> getMessage(
+            @RequestParam(required = false) Boolean isNew,
+            @RequestParam(required = false) Boolean isFavorite
+    ) {
+        return messageService.getMessages(isNew, isFavorite);
+    }
+
+    @PostMapping
+    public MessageResponse createMessage(@RequestBody CreateMessageRequest request) {
+        return messageService.createMessage(request);
+    }
+
+    @PatchMapping("/{id}")
+    public MessageResponse updateMessage(
+            @PathVariable UUID id,
+            @RequestBody UpdateMessageRequest request
+        ) {
+        return messageService.updateMessage(id, request);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteMessage(@PathVariable UUID id) {
+        messageService.deleteMessage(id);
+    }
+}
