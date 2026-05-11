866ad68c7beb7a8b24626fce7183f62c423b7341
jeperico <joaoeperico@gmail.com>
2026-05-11 15:22:40 -0300

    feat: implements authorization on all endpoints

--- src/main/java/com/hestia/api/infraestructure/security/config/SecurityConfig.java
+++ src/main/java/com/hestia/api/infraestructure/security/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.hestia.api.infraestructure.security.filter.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -28,10 +29,53 @@ public class SecurityConfig {
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
                 .authorizeHttpRequests(auth -> auth
+                        // SWAGGER
                         .requestMatchers(
                                 "/swagger-ui/**",
                                 "/v3/api-docs/**"
                         ).permitAll()
+                        // ACCOUNT
+                        .requestMatchers(HttpMethod.PATCH,
+                                "/account/**"
+                        ).hasRole("COUPLE")
+                        .requestMatchers(
+                                "/account",
+                                "/account/**"
+                        ).hasRole("ADMIN")
+                        // MESSAGE
+                        .requestMatchers(HttpMethod.POST,
+                                "/message"
+                        ).permitAll()
+                        .requestMatchers(
+                                "/message",
+                                "/message/**"
+                        ).hasRole("COUPLE")
+                        // REGISTRY
+                        .requestMatchers(HttpMethod.GET,
+                                "/gift",
+                                "/gift/**"
+                        ).permitAll()
+                        .requestMatchers(
+                                "/gift",
+                                "/gift/**"
+                        ).hasRole("COUPLE")
+                        // RSVP
+                        .requestMatchers(HttpMethod.POST,
+                                "/rsvp/invite/search"
+                        ).permitAll()
+                        .requestMatchers(HttpMethod.GET,
+                                "/rsvp/guest"
+                        ).permitAll()
+                        .requestMatchers(HttpMethod.PATCH,
+                                "/rsvp/guest/status/**"
+                        ).permitAll()
+                        .requestMatchers(
+                                "/rsvp/invite",
+                                "/rsvp/invite/**",
+                                "rsvp/guest",
+                                "rsvp/guest/**"
+                        ).hasRole("COUPLE")
+                        // GENERAL
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
