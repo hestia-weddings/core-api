4811364c43e4232c181b664cf204ccf3ec943edd
jeperico <joaoeperico@gmail.com>
2026-05-26 15:18:55 -0300

    feat: create asaas client tests

--- src/main/java/com/hestia/api/infrastructure/asaas/dto/AsaasCheckoutRequest.java
+++ src/main/java/com/hestia/api/infrastructure/asaas/dto/AsaasCheckoutRequest.java
@@ -1,10 +1,10 @@
 package com.hestia.api.infrastructure.asaas.dto;
 
+import java.util.List;
+
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
-
-import java.util.List;
 
 @Getter
 @Setter
